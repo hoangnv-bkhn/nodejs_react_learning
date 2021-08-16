cee2589b7e7633aba2148277854cae11b2282e55
bw_flow09 <viethoang.ma1009@gmail.com>
2021-08-16 12:27:48 +0700

    HTTPS

--- authenticate.js
+++ authenticate.js
@@ -13,8 +13,8 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-exports.getToken = function(user) {
-    return jwt.sign(user, config.secretKey, {expiresIn: 3600});
+exports.getToken = function (user) {
+    return jwt.sign(user, config.secretKey, { expiresIn: 3600 });
 }
 
 var opts = {};
@@ -23,15 +23,25 @@ opts.secretOrKey = config.secretKey;
 
 exports.jwtPassport = passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
     console.log('JWT payload: ', jwt_payload);
-    User.findOne({_id: jwt_payload._id}, (err, user) => {
+    User.findOne({ _id: jwt_payload._id }, (err, user) => {
         if (err) {
             return done(err, false);
         } else if (user) {
             return done(null, user);
         } else {
-            return done(null,false);
+            return done(null, false);
         }
     })
 }))
 
-exports.verifyUser = passport.authenticate('jwt', {session: false});
+exports.verifyUser = passport.authenticate('jwt', { session: false });
+
+exports.verifyAdmin = (req, res, next) => {
+    if (req != null && req.user.admin) {
+        next();
+    } else {
+        err = new Error('You are not authorized to perform this operation!');
+        err.status = 403;
+        return next(err);
+    }
+};
--- models/user.js
+++ models/user.js
@@ -6,6 +6,14 @@ var User = new Schema({
     admin: {
         type: Boolean,
         default: false
+    },
+    firstname: {
+        type: String,
+        default: ''
+    },
+    lastname: {
+        type: String,
+        default: ''
     }
 });
 
