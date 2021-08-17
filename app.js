38b11e7c13c411da393796c47a3bd05c0f48a06b
bw_flow09 <viethoang.ma1009@gmail.com>
2021-08-17 07:42:17 +0700

    File Upload

--- app.js
+++ app.js
@@ -12,13 +12,12 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var uploadRouter = require('./routes/uploadRouter');
 
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 
 const mongoose = require('mongoose');
-
-const Dishes = require('./models/dishes');
 
 const url = config.mongoUrl;
 
@@ -75,6 +74,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
