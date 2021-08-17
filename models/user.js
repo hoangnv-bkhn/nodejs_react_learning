b33423594d566eed17e46b7f981ee04ff6376e35
bw_flow09 <viethoang.ma1009@gmail.com>
2021-08-17 13:09:33 +0700

    Passport Facebook

--- models/user.js
+++ models/user.js
@@ -14,7 +14,8 @@ var User = new Schema({
     lastname: {
         type: String,
         default: ''
-    }
+    },
+    facebookId: String
 });
 
 User.plugin(passportLocalMongoose)
--- routes/users.js
+++ routes/users.js
@@ -7,17 +7,17 @@ var User = require('../models/user')
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/',cors.corsWithOptions,  authenticate.verifyUser, authenticate.verifyAdmin, function (req, res, next) {
+router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, function (req, res, next) {
   User.find({})
-  .then((users) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type','application/json');
-    res.json(users);
-  }, (err) => next(err))
-  .catch((err) => next(err));
+    .then((users) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(users);
+    }, (err) => next(err))
+    .catch((err) => next(err));
 });
 
-router.post('/signup',cors.corsWithOptions,  (req, res, next) => {
+router.post('/signup', cors.corsWithOptions, (req, res, next) => {
   User.register(new User({ username: req.body.username }), req.body.password, (err, user) => {
     if (err) {
       res.statusCode = 500;
@@ -48,18 +48,31 @@ router.post('/signup',cors.corsWithOptions,  (req, res, next) => {
   });
 });
 
-router.post('/login',cors.corsWithOptions,  passport.authenticate('local'), (req, res) => {
+router.post('/login', cors.corsWithOptions, passport.authenticate('local'), (req, res) => {
   var token = authenticate.getToken({ _id: req.user._id });
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.json({ success: true, token: token, status: 'You are successfully logged in!' });
 });
 
-router.get('/logout',cors.corsWithOptions,  (req, res, next) => {
+router.get('/logout', cors.corsWithOptions, (req, res, next) => {
   if (req.session) {
     req.session.destroy();
     res.clearCookie('session-id');
     res.redirect('/');
+  } else {
+    var err = new Error('You are not logged in!');
+    err.status = 403;
+    next(err);
+  }
+});
+
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+  if (req.user) {
+    var token = authenticate.getToken({ _id: req.user._id });
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({ success: true, token: token, status: 'You are successfully logged in!' });
   } else {
     var err = new Error('You are not logged in!');
     err.status = 403;
