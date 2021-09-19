a610ea5f09c4ce9a8d7242ba6b6d2450982db3c8
Nguyen Viet Hoang <58356409+hoangnv-bkhn@users.noreply.github.com>
2021-09-19 14:36:33 +0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion',
-    'facebook': {
-        clientID: '1504861413201225',
-        clientSecret: '67453743054d7bd8569a79445e1cb688'
-    }
-}
